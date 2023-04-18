9af33b281849e4e8867f18bfd8d57b902c29e54f
yuanming <1985161994@qq.com>
2023-04-18 13:42:58 +0800

    change the latenct as 5 min, threshold as 2

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -436,7 +436,7 @@ public class CassandraDaemon {
 
         // schedule periodic send sstable content task submission
         ScheduledExecutors.optionalTasks.scheduleWithFixedDelay(
-                ColumnFamilyStore.getSendSSTRunnable("ycsb", "usertable", DatabaseDescriptor.getCompactionThreshold(), 2),
+                ColumnFamilyStore.getSendSSTRunnable("ycsb", "usertable", DatabaseDescriptor.getCompactionThreshold(), 5),
                 1, 1, TimeUnit.MINUTES);
         
 
