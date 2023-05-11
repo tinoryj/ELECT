4838dd9d4c37d797b62ce81eb29dfe09b5dcc252
yuanming <1985161994@qq.com>
2023-05-11 14:56:04 +0800

    debug Transactional

--- src/java/org/apache/cassandra/utils/concurrent/Transactional.java
+++ src/java/org/apache/cassandra/utils/concurrent/Transactional.java
@@ -138,6 +138,7 @@ public interface Transactional extends AutoCloseable
                 throw new IllegalStateException(String.format("Cannot commit unless READY_TO_COMMIT; state is {%s}, isRewrite {%s}", state, isRewrite));
             accumulate = doCommit(accumulate, ecSSTable);
             accumulate = doPostCleanup(accumulate);
+            logger.debug("rymDebug: successful commit ec_metadata {}", ecSSTable.descriptor);
             state = State.COMMITTED;
             return accumulate;
         }
