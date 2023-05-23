16be0018731f8b5eec498c0ca8a574dfd6109025
yuanming <1985161994@qq.com>
2023-05-23 15:08:39 +0800

    debug the update new sstable method

--- src/java/org/apache/cassandra/db/compaction/CompactionTask.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionTask.java
@@ -358,7 +358,7 @@ public class CompactionTask extends AbstractCompactionTask {
                     ecSSTable.SetIsReplicationTransferredToErasureCoding();
                     logger.debug("rymDebug: this is rewrite SSTable method, replacing SSTable {}", ecSSTable.descriptor);
                     transaction.update(ecSSTable, false);
-                    transaction.checkpoint();
+                    // transaction.checkpoint();
                     newSSTables = writer.finish(ecSSTable);
                     logger.debug("[Rewrite SSTables]: rewrite SSTable is FINISHED, ecSSTable is {},", ecSSTable.descriptor);
                     // TODO: re-create sstable reader from ecmetadata 
