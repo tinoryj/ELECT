a88c7f55d0182523f864f25a71f408c4b321a126
yuanming <1985161994@qq.com>
2023-05-25 14:05:28 +0800

    unmask update code for debug

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -1824,9 +1824,9 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
                 logger.error("rymERROR: set IsReplicationTransferredToErasureCoding failed!");
             }
             logger.debug("rymDebug: this is replace SSTable method, replacing SSTable {}", ecSSTable.descriptor);
-            // txn.update(ecSSTable, false);
-            // // ecSSTable.setupOnline();
-            // txn.checkpoint();
+            txn.update(ecSSTable, false);
+            // ecSSTable.setupOnline();
+            txn.checkpoint();
             // txn.tracker.apply(View.updateLiveSet(Collections.emptySet(), Collections.singleton(ecSSTable)));
             // txn.prepareToCommit(ecSSTable);
             maybeFail(txn.commitEC(null, ecSSTable, false));
