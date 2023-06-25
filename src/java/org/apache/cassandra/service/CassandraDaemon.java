b77168bc9fe4e12a8d6f8866b7d7f44015006120
yuanming <1985161994@qq.com>
2023-06-25 23:38:32 +0800

    [Debug] Modify the parameter

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -471,7 +471,7 @@ public class CassandraDaemon {
 
         // schedule periodical tasks of force compaction the last level
         ScheduledExecutors.optionalTasks.scheduleWithFixedDelay(ColumnFamilyStore.getForceCompactionForTheLastLevelRunnable(),
-                                                                DatabaseDescriptor.getInitialDelay(),
+                                                                420,
                                                                 5,
                                                                 TimeUnit.MINUTES);
 
