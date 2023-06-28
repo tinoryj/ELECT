7fd6aa50c07926ef070fbab35bcce6b81556355a
yuanming <1985161994@qq.com>
2023-06-28 10:40:52 +0800

    [Debug] Modify parameters

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -442,7 +442,7 @@ public class CassandraDaemon {
         ScheduledExecutors.optionalTasks.scheduleWithFixedDelay(
             ColumnFamilyStore.getSendSSTRunnable("ycsb", "usertable", LeveledGenerations.getMaxLevelCount() - 1, DatabaseDescriptor.getTaskDelay()),
                                                  DatabaseDescriptor.getInitialDelay(),
-                                                 1,
+                                                 DatabaseDescriptor.getTaskDelay(),
                                                  TimeUnit.MINUTES);
 
         // schedule periodical tasks of erasure coding
