2dfb4361ceed9278d7aff77a33306165aac8e1a0
yuanming <1985161994@qq.com>
2023-07-10 20:41:49 +0800

    Debug ansible

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,14 +23,14 @@ func() {
     coordinator=$1
     operationcount=$2
     threads=$3
-    mkdir -p logs/run-log/
     file_name="$(date +%s)-${operationcount}-${threads}"
     cd ~/ycsb-0.17.0
+    mkdir -p logs/run-log/
     
     sed -i "s/operationcount=.*$/operationcount=${operationcount}/" workloads/workload_template
     
     
-    bin/ycsb run cassandra-cql -p hosts=$coordinator -p cassandra.readconsistencylevel="ALL" -threads $threads -s -P workloads/workload_template > logs/insert-log/${file_name}.log 2>&1
+    bin/ycsb run cassandra-cql -p hosts=$coordinator -p cassandra.readconsistencylevel="ALL" -threads $threads -s -P workloads/workload_template > logs/run-log/${file_name}.log 2>&1
 }
 
 func "$1" "$2" "$3"
