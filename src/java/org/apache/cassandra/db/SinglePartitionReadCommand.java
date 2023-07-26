7c8745061a993f77e0d0c50cbabd7f6f9c57f0cf
yuanming <1985161994@qq.com>
2023-07-26 13:26:43 +0800

    Add logs for selected sstables during read

--- src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
+++ src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
@@ -718,6 +718,18 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
              * read a partition tombstone.
              */
             view.sstables.sort(SSTableReader.maxTimestampDescending);
+
+            String sstablesHashID = "", sstablesPath = "";
+            for (SSTableReader sstable : view.sstables) {
+                sstablesHashID += ("\t" + sstable.getSSTableHashID());
+                sstablesPath += ("\t" + sstable.getFilename());
+            }
+            logger.debug("[Tinoryj] the target sstables for key token = {}, raw key = {}, include: [{},{}] sstables",
+                    ((SinglePartitionReadCommand) controller.command).partitionKey().getToken(),
+                    ((SinglePartitionReadCommand) controller.command).partitionKey()
+                            .getRawKey(((SinglePartitionReadCommand) controller.command).metadata()),
+                    sstablesHashID, sstablesPath);
+
             int nonIntersectingSSTables = 0;
             int includedDueToTombstones = 0;
 
@@ -846,7 +858,8 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
             if (readRecoveryedSSTableCount != 0 && inputCollector.isEmpty()) {
                 logger.error(
                         "[Tinoryj-ERROR] Read recoveryed sstable count = {}, target sstable number in view = {}, but get no data from them. The SSTables list = {}, hash list = {}, target read key token = {}, raw key = {}",
-                        readRecoveryedSSTableCount,targetSSTableSetSize, readRecoveryedSSTableList, readRecoveryedSSTableHashList,
+                        readRecoveryedSSTableCount, targetSSTableSetSize, readRecoveryedSSTableList,
+                        readRecoveryedSSTableHashList,
                         ((SinglePartitionReadCommand) controller.command).partitionKey().getToken(),
                         ((SinglePartitionReadCommand) controller.command).partitionKey()
                                 .getRawKey(((SinglePartitionReadCommand) controller.command).metadata()));
@@ -968,6 +981,16 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
 
         /* add the SSTables on disk */
         view.sstables.sort(SSTableReader.maxTimestampDescending);
+        String sstablesHashID = "", sstablesPath = "";
+        for (SSTableReader sstable : view.sstables) {
+            sstablesHashID += ("\t" + sstable.getSSTableHashID());
+            sstablesPath += ("\t" + sstable.getFilename());
+        }
+        logger.debug("[Tinoryj] the target sstables for key token = {}, raw key = {}, include: [{},{}] sstables",
+                ((SinglePartitionReadCommand) controller.command).partitionKey().getToken(),
+                ((SinglePartitionReadCommand) controller.command).partitionKey()
+                        .getRawKey(((SinglePartitionReadCommand) controller.command).metadata()),
+                sstablesHashID, sstablesPath);
         // read sorted sstables
 
         int readRecoveryedSSTableCount = 0, targetSSTableSetSize = 0;
