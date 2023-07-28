7f8ff54d0872e78be56a17de22292eb777fe7ae5
tinoryj <tinoryj@gmail.com>
2023-07-28 20:03:40 +0800

    Update OSSAccess

--- src/java/org/apache/cassandra/io/erasurecode/alibaba/OSSAccess.java
+++ src/java/org/apache/cassandra/io/erasurecode/alibaba/OSSAccess.java
@@ -211,11 +211,11 @@ public class OSSAccess {
                 System.out.println("\tCreate Time: " + info.getBucket().getCreationDate());
                 System.out.println("\tUser Info: " + info.getBucket().getOwner());
 
-                ossAccess.uploadFileToOSS("/home/tinoryj/Projects/CassandraEC/README.md");
-                ossAccess.downloadFileFromOSS("/home/tinoryj/Projects/CassandraEC/README.md",
-                        "/home/tinoryj/Projects/CassandraEC/README.md.d");
-                ossAccess.downloadFileFromOSS("/home/tinoryj/Projects/CassandraEC/README.md",
-                        "/home/tinoryj/Projects/CassandraEC/README.md.d");
+                // ossAccess.uploadFileToOSS("/home/tinoryj/Projects/CassandraEC/README.md");
+                // ossAccess.downloadFileFromOSS("/home/tinoryj/Projects/CassandraEC/README.md",
+                // "/home/tinoryj/Projects/CassandraEC/README.md.d");
+                // ossAccess.downloadFileFromOSS("/home/tinoryj/Projects/CassandraEC/README.md",
+                // "/home/tinoryj/Projects/CassandraEC/README.md.d");
                 ossAccess.cleanUpOSS();
             } else {
                 System.out.println("Bucket not exist, creating：" + bucketName);
