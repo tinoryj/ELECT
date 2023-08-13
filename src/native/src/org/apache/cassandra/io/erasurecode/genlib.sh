ed649b4852b3d80f991853f990a70da45cd66e5e
tinoryj <tinoryj@gmail.com>
2023-08-13 17:40:39 +0800

    Update genlib

--- src/native/src/org/apache/cassandra/io/erasurecode/genlib.sh
+++ src/native/src/org/apache/cassandra/io/erasurecode/genlib.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Generate libec.so
