277b1b4dc90260548c68cd89b660a23804b01898
renyuanming <1985161994@qq.com>
2023-08-16 15:05:02 +0800

    add access frequency command for nodetool

--- src/java/org/apache/cassandra/tools/NodeTool.java
+++ src/java/org/apache/cassandra/tools/NodeTool.java
@@ -90,6 +90,7 @@ public class NodeTool {
 
     public int execute(String... args) {
         List<Class<? extends NodeToolCmdRunnable>> commands = newArrayList(
+                AccessFrequency.class,
                 Assassinate.class,
                 CassHelp.class,
                 CfHistograms.class,
