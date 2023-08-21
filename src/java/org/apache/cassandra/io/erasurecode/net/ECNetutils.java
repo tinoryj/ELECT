f3de481ead90e5b216f5cef03da274ee6c16154b
renyuanming <1985161994@qq.com>
2023-08-21 16:50:53 +0800

    Debug

--- src/java/org/apache/cassandra/io/erasurecode/net/ECNetutils.java
+++ src/java/org/apache/cassandra/io/erasurecode/net/ECNetutils.java
@@ -87,7 +87,7 @@ public final class ECNetutils {
     private static final String dataDir = System.getProperty("user.dir")+"/data/data/";
     private static final String localParityCodeDir = System.getProperty("user.dir")+"/data/localParityHashes/";
     private static final String scriptsDir = System.getProperty("user.dir")+"/scripts/";
-    private static final String inMemoryDataBackupDir = System.getProperty("user.dir")+"/inMemoryData/";
+    private static final String inMemoryDataBackupDir = System.getProperty("user.dir")+"/data/inMemoryData/";
     private static final int MIGRATION_RETRY_COUNT = 5;
 
     public static class ByteObjectConversion {
