24de3eb57a7e599f811791db51b37162cde3c00a
renyuanming <1985161994@qq.com>
2023-08-22 12:49:57 +0800

    Debug

--- src/java/org/apache/cassandra/io/erasurecode/net/ECRequestParityVerbHandler.java
+++ src/java/org/apache/cassandra/io/erasurecode/net/ECRequestParityVerbHandler.java
@@ -61,6 +61,7 @@ public class ECRequestParityVerbHandler implements IVerbHandler<ECRequestParity>
             if (Files.exists(path)) {
                 break;
             } else if (StorageService.ossAccessObj.downloadFileAsByteArrayFromOSS(localParityCodeDir + parityHash, firstParityNode)) {
+                path = Paths.get(localParityCodeDir + parityHash);
                 break;
             }
             else {
