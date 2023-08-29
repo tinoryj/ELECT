e66418ea2bb529d477dd069163d4ab18ae760de0
renyuanming <1985161994@qq.com>
2023-08-29 14:14:32 +0800

    Debug migration

--- src/java/org/apache/cassandra/io/erasurecode/alibaba/OSSAccess.java
+++ src/java/org/apache/cassandra/io/erasurecode/alibaba/OSSAccess.java
@@ -162,7 +162,7 @@ public class OSSAccess implements AutoCloseable {
         return true;
     }
 
-    public boolean downloadFileAsByteArrayFromOSS(String originalFilePath, String targetIp) {
+    public synchronized boolean downloadFileAsByteArrayFromOSS(String originalFilePath, String targetIp) {
 
         String objectName = originalFilePath.replace('/', '_') + "_" + targetIp;
 
