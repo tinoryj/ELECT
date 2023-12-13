96a63088b0af9d1847dfe5e1cdf672dc30f621cb
tinoryj <tinoryj@gmail.com>
2023-12-13 17:25:45 +0800

    Debug local cold tier

--- Prototype/src/java/org/apache/cassandra/io/erasurecode/alibaba/OSSAccess.java
+++ Prototype/src/java/org/apache/cassandra/io/erasurecode/alibaba/OSSAccess.java
@@ -26,6 +26,8 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,29 +45,32 @@ import org.apache.cassandra.utils.FBUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.aliyun.oss.ClientBuilderConfiguration;
-import com.aliyun.oss.ClientException;
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.common.auth.*;
-import com.aliyun.oss.common.comm.Protocol;
-import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.OSSException;
-import com.aliyun.oss.model.PutObjectRequest;
-import com.aliyun.oss.model.PutObjectResult;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.aliyun.oss.model.BucketInfo;
-import com.aliyun.oss.model.DeleteObjectsRequest;
-import com.aliyun.oss.model.DeleteObjectsResult;
-import com.aliyun.oss.model.GetObjectRequest;
-import com.aliyun.oss.model.OSSObject;
-import com.aliyun.oss.model.OSSObjectSummary;
-import com.aliyun.oss.model.ObjectListing;
-import com.aliyun.oss.model.ObjectMetadata;
+// import com.aliyun.oss.ClientBuilderConfiguration;
+// import com.aliyun.oss.ClientException;
+// import com.aliyun.oss.OSS;
+// import com.aliyun.oss.common.auth.*;
+// import com.aliyun.oss.common.comm.Protocol;
+// import com.aliyun.oss.OSSClientBuilder;
+// import com.aliyun.oss.OSSException;
+// import com.aliyun.oss.model.PutObjectRequest;
+// import com.aliyun.oss.model.PutObjectResult;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.aliyun.oss.model.BucketInfo;
+// import com.aliyun.oss.model.DeleteObjectsRequest;
+// import com.aliyun.oss.model.DeleteObjectsResult;
+// import com.aliyun.oss.model.GetObjectRequest;
+// import com.aliyun.oss.model.OSSObject;
+// import com.aliyun.oss.model.OSSObjectSummary;
+// import com.aliyun.oss.model.ObjectListing;
+// import com.aliyun.oss.model.ObjectMetadata;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.Socket;
 import java.net.URLDecoder;
 
 // The following is the code for using Alibaba OSS as the cold tier.
@@ -360,246 +365,123 @@ public class OSSAccess implements AutoCloseable {
     public void close() throws Exception {
     }
 
-    public boolean uploadFileToOSS(String targetFilePath) {
+    public boolean uploadFileToOSS(String targetFilePath, byte[] content) {
         String objectName = targetFilePath.replace('/', '_') + localIP;
         logger.debug("ELECT-Debug: target object name is ({})", objectName);
-        try {
-            InputStream inputStream = new FileInputStream(targetFilePath);
-            // 创建PutObjectRequest对象。
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
-            // 创建PutObject请求。
-            long startTime = System.currentTimeMillis();
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-            long timeCost = System.currentTimeMillis() - startTime;
-            StorageService.instance.migratedRawSSTableTimeSendCost += timeCost;
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId() + "\nRequest object key:" + objectName);
-            return false;
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-            return false;
-        } catch (Throwable e) {
-            logger.error("Get file input stream error:");
+        try (Socket socket = new Socket(DatabaseDescriptor.getColdTierIP(), DatabaseDescriptor.getColdTierPort());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream())) {
+            long startUploadTime = System.currentTimeMillis();
+            objectOutputStream.writeUTF("UPLOAD");
+            objectOutputStream.writeUTF(objectName);
+            objectOutputStream.writeObject(content);
+            objectOutputStream.flush();
+            long timeCost = System.currentTimeMillis() - startUploadTime;
+            logger.debug(
+                    "[ELECT-Debug]: upload file to OSS cost time is ({}ms), the file size is ({}), file name is ({})",
+                    timeCost, content.length, targetFilePath);
+            return objectInputStream.readBoolean(); // Wait and read the response
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-        return true;
-    }
-
-    public boolean uploadFileToOSS(String targetFilePath, byte[] content) {
-        String objectName = targetFilePath.replace('/', '_') + localIP;
-
-        try {
-            long startUploadTime = System.currentTimeMillis();
-            logger.debug("ELECT-Debug: target file path is ({}), content size is ({}), object name is ({})",
-                    targetFilePath, content.length, objectName);
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName,
-                    new ByteArrayInputStream(content));
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-            long timeCost = System.currentTimeMillis() - startUploadTime;
-            logger.debug(
-                    "ELECT-Debug: upload file to OSS cost time is ({}ms), the file size is ({}), file name is ({})",
-                    timeCost, content.length, targetFilePath);
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId() + "\nRequest object key:" + objectName);
-            return false;
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-            return false;
-        }
-        return true;
     }
 
     public boolean uploadFileToOSS(String targetFilePath, String content) {
-        String objectName = targetFilePath.replace('/', '_') + localIP;
+        return uploadFileToOSS(targetFilePath, content.getBytes());
+    }
+
+    public boolean uploadFileToOSS(String targetFilePath) {
         try {
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName,
-                    new ByteArrayInputStream(content.getBytes()));
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId() + "\nRequest object key:" + objectName);
-            return false;
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
+            long startTime = System.currentTimeMillis();
+            byte[] content = Files.readAllBytes(Paths.get(targetFilePath));
+            return uploadFileToOSS(targetFilePath, content);
+            long timeCost = System.currentTimeMillis() - startTime;
+            StorageService.instance.migratedRawSSTableTimeSendCost += timeCost;
+        } catch (IOException e) {
+            e.printStackTrace();
             return false;
         }
-        return true;
     }
 
     public boolean downloadFileFromOSS(String originalFilePath, String targetStorePath) {
         String objectName = originalFilePath.replace('/', '_') + localIP;
-        try {
-            ossClient.getObject(
-                    new GetObjectRequest(bucketName, objectName),
-                    new File(targetStorePath));
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId() + "\nRequest object key:" + objectName);
-            return false;
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
+        try (Socket socket = new Socket(DatabaseDescriptor.getColdTierIP(), DatabaseDescriptor.getColdTierPort());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream())) {
+
+            objectOutputStream.writeUTF("DOWNLOAD");
+            objectOutputStream.writeUTF(objectName);
+            objectOutputStream.flush();
+            boolean fileExists = objectInputStream.readBoolean();
+            if (fileExists) {
+                byte[] content = (byte[]) objectInputStream.readObject();
+                Files.write(Paths.get(targetStorePath), content);
+                return true;
+            } else {
+                logger.error(
+                        "[ELECT-ERROR] Could not find target object from the cold tier, object name = " + objectName);
+                return false;
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            logger.error(
+                    "[ELECT-ERROR] Could not connected to the cold tier, object name = " + objectName);
             return false;
         }
-        return true;
     }
 
     public boolean downloadFileAsByteArrayFromOSS(String originalFilePath, String targetIp) {
         String objectName = originalFilePath.replace('/', '_') + "_" + targetIp;
-        try {
+        try (Socket socket = new Socket(DatabaseDescriptor.getColdTierIP(), DatabaseDescriptor.getColdTierPort());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream())) {
             semaphore.acquire();
-            try {
-                // FileUtils.delete(originalFilePath);
-                // Varify the file exist or not
-                ossClient.getObject(
-                        new GetObjectRequest(bucketName, objectName),
-                        new File(originalFilePath));
-            } catch (OSSException oe) {
-                logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                        + "\nRequest ID:" + oe.getRequestId() + "\nRequest object key:" + objectName);
-                logger.error("[ELECT-ERROR]: Download original file from OSS failed, file name is ({})",
-                        objectName);
-                return false;
-            } catch (ClientException ce) {
-                logger.error("OSS Internet Error Message:" + ce.getMessage());
-                logger.error("[ELECT-ERROR]: Download original file from OSS failed, file name is ({})",
-                        objectName);
+
+            objectOutputStream.writeUTF("DOWNLOAD");
+            objectOutputStream.writeUTF(objectName);
+            objectOutputStream.flush();
+            System.out.println("Try to download from server: " + objectName + ", request sent");
+            boolean fileExists = objectInputStream.readBoolean();
+            System.out.println("Recv the file exist flag");
+            if (fileExists) {
+                byte[] content = (byte[]) objectInputStream.readObject();
+                Files.write(Paths.get(originalFilePath), content);
+                return true;
+            } else {
+                System.out.println("File not found on server: " + objectName);
                 return false;
             }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            logger.error("[ELECT-ERROR]: Download original file from OSS failed, file name is ({})", objectName);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
             return false;
         } finally {
             semaphore.release();
         }
-        logger.debug("ELECT-Debug: Downloaded original file from OSS, file name is ({})", objectName);
+        logger.debug("[ELECT-Debug]: Downloaded original file from cold tier, file name is ({})", objectName);
         return true;
-    }
-
-    public boolean deleteSingleFileInOSS(String targetFilePath) {
-        String objectName = targetFilePath.replace('/', '_') + localIP;
-        try {
-            boolean found = ossClient.doesObjectExist(bucketName, objectName);
-            if (found) {
-                logger.debug("Found target file " + targetFilePath + " in bucket, delete it now");
-                ossClient.deleteObject(bucketName,
-                        targetFilePath.replace('/', '_') + localIP);
-                return true;
-            } else {
-                logger.error("Could not found target file " + targetFilePath + " in bucket");
-                return false;
-            }
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId() + "\nRequest object key:" + objectName);
-            return false;
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-            return false;
-        }
-    }
-
-    public void listingFilesInOSS() {
-        // Listing all files from OSS bucket
-        try {
-            ObjectListing objectListing = ossClient.listObjects(bucketName);
-            List<OSSObjectSummary> sums = objectListing.getObjectSummaries();
-            List<String> keys = new ArrayList<String>();
-            for (OSSObjectSummary s : sums) {
-                logger.debug("Existing file on OSS: {}", s.getKey());
-                keys.add(s.getKey());
-            }
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId());
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-        }
-    }
-
-    public void cleanUpOSS() {
-        // Delete all files from OSS bucket
-        try {
-            while (true) {
-                ObjectListing objectListing = ossClient.listObjects(bucketName);
-                List<OSSObjectSummary> sums = objectListing.getObjectSummaries();
-                List<String> keys = new ArrayList<String>();
-                for (OSSObjectSummary s : sums) {
-                    logger.debug("Existing file on OSS: {}", s.getKey());
-                    keys.add(s.getKey());
-                }
-                if (keys.isEmpty()) {
-                    logger.debug("No file on OSS, nothing to delete");
-                    break;
-                } else {
-                    DeleteObjectsResult deleteObjectsResult = ossClient
-                            .deleteObjects(new DeleteObjectsRequest(bucketName).withKeys(keys).withEncodingType("url"));
-                    List<String> deletedObjects = deleteObjectsResult.getDeletedObjects();
-                    try {
-                        for (String obj : deletedObjects) {
-                            String deleteObj = URLDecoder.decode(obj, "UTF-8");
-                            logger.debug("Delete file: {}", deleteObj);
-                        }
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId());
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-        }
-    }
-
-    public void checkBucketStatusAndCreateBucketIfNotExist() {
-        try {
-            // Check if the bucket exists then update/download/delete.
-            if (!ossClient.doesBucketExist(bucketName)) {
-                logger.debug("Bucket not exist, creating：{}", bucketName);
-                ossClient.createBucket(bucketName);
-            }
-            BucketInfo info = ossClient.getBucketInfo(bucketName);
-            logger.debug(
-                    "Target Bucket is created：{}\n\tBucket Info:\n\tData Center: {}\n\tUser Info: {}\n\tCreate Time: {}",
-                    bucketName, info.getBucket().getLocation(), info.getBucket().getOwner(),
-                    info.getBucket().getCreationDate());
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId());
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-        } finally {
-            if (ossClient != null) {
-                logger.debug("OSS Client is start and connected");
-            }
-        }
     }
 
     public static void main(String[] args) throws Exception {
         OSSAccess ossAccess = new OSSAccess();
-        try {
-            ossAccess.cleanUpOSS();
-            // byte[] content = "Hello OSS".getBytes();
-            // ossAccess.uploadFileToOSS("test.md",
-            // content);
-            // ossAccess.downloadFileFromOSS("test.md",
-            // "test.md.d");
-        } catch (OSSException oe) {
-            logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" + oe.getErrorCode()
-                    + "\nRequest ID:" + oe.getRequestId());
-        } catch (ClientException ce) {
-            logger.error("OSS Internet Error Message:" + ce.getMessage());
-        } finally {
-            if (ossAccess != null) {
-                ossAccess.cleanUpOSS();
-                System.out.println("OSS cleanup and closed.");
-            }
-        }
+        // try {
+        // ossAccess.cleanUpOSS();
+        // // byte[] content = "Hello OSS".getBytes();
+        // // ossAccess.uploadFileToOSS("test.md",
+        // // content);
+        // // ossAccess.downloadFileFromOSS("test.md",
+        // // "test.md.d");
+        // } catch (OSSException oe) {
+        // logger.error("OSS Error Message:" + oe.getErrorMessage() + "\nError Code:" +
+        // oe.getErrorCode()
+        // + "\nRequest ID:" + oe.getRequestId());
+        // } catch (ClientException ce) {
+        // logger.error("OSS Internet Error Message:" + ce.getMessage());
+        // } finally {
+        // if (ossAccess != null) {
+        // ossAccess.cleanUpOSS();
+        // System.out.println("OSS cleanup and closed.");
+        // }
+        // }
     }
 }
