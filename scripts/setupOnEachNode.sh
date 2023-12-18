282fcae5672c1c44b31d8bac115e35df623311a2
tinoryj <tinoryj@gmail.com>
2023-12-18 21:00:38 +0800

    Update scripts temp

--- scripts/setupOnEachNode.sh
+++ scripts/setupOnEachNode.sh
@@ -6,7 +6,12 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -q -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa
 fi
 # SSH key-free connection from control node to all nodes
-for nodeIP in "${FullNodeList[@]}"; do
+# SSH key-free connection from control node to all nodes
+for nodeIP in "${NodesList[@]}" "${OSSServerNode}" "${ClientNode}"; do
+    ssh-keyscan -H ${nodeIP} >> ~/.ssh/known_hosts
+done
+
+for nodeIP in "${NodesList[@]}" "${OSSServerNode}" "${ClientNode}"; do
     echo "Set SSH key-free connection to node ${nodeIP}"
     ssh-copy-id -i ~/.ssh/id_rsa.pub ${UserName}@${nodeIP}
 done
