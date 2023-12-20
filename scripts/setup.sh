84c1cd6623c2d428b8d48b8f083d191c0d1fba0a
tinoryj <tinoryj@gmail.com>
2023-12-20 14:45:43 +0800

    Test on CC

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,17 +8,21 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -q -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa
 fi
 # SSH key-free connection from control node to all nodes
+
 for nodeIP in "${NodesList[@]}" "${OSSServerNode}" "${ClientNode}"; do
-    ssh-keyscan -H ${nodeIP} >>~/.ssh/known_hosts
+    if [ ${UserName} == "cc" ]; then
+        scp ~/.ssh/config cc@${nodeIP}:~/.ssh/
+        scp ~/.ssh/id_rsa cc@${nodeIP}:~/.ssh/
+    else
+        echo "Set SSH key-free connection to node ${nodeIP}"
+        ssh-keyscan -H ${nodeIP} >>~/.ssh/known_hosts
+        ssh-copy-id -i ~/.ssh/id_rsa.pub ${UserName}@${nodeIP}
+    fi
 done
 
 for nodeIP in "${NodesList[@]}" "${OSSServerNode}" "${ClientNode}"; do
-    echo "Set SSH key-free connection to node ${nodeIP}"
-    ssh-copy-id -i ~/.ssh/id_rsa.pub ${UserName}@${nodeIP}
-done
-
-for nodeIP in "${NodesList[@]}" "${OSSServerNode}" "${ClientNode}"; do
-    rsync -av --progress ~/ELECT ${UserName}@${nodeIP}:~/
+    ssh ${UserName}@${nodeIP} "rm -rf ${PathToArtifact}"
+    scp -r ${PathToArtifact} ${UserName}@${nodeIP}:~/
 done
 
 # Install packages
@@ -26,8 +30,8 @@ if [ ! -z "${sudoPasswd}" ]; then
     printf ${sudoPasswd} | sudo -S apt-get update
     printf ${sudoPasswd} | sudo -S apt-get install -y ant maven clang llvm python3 ansible python3-pip libisal-dev openjdk-11-jdk openjdk-11-jre
 else
-    sudo -S apt-get update
-    sudo -S apt-get install -y ant maven clang llvm python3 ansible python3-pip libisal-dev openjdk-11-jdk openjdk-11-jre
+    sudo apt-get update
+    sudo apt-get install -y ant maven clang llvm python3 ansible python3-pip libisal-dev openjdk-11-jdk openjdk-11-jre
 fi
 
 pip install cassandra-driver
