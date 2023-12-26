eb390e6fa4d35836751fdf2b06a3abcba311bc00
tinoryj <tinoryj@gmail.com>
2023-12-26 19:12:18 +0800

    Update configuration for cuhk testbed

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 . /etc/profile
 # Common params for all experiments
-NodesList=(10.31.0.190 10.31.0.180 10.31.0.189 10.31.0.184 10.31.0.182 10.31.0.188) # The IP addresses of the ELECT cluster nodes
-OSSServerNode="10.31.0.186" # The IP address of the OSS server node
+NodesList=(192.168.0.21 192.168.0.22 192.168.0.23 192.168.0.25 192.168.0.26 192.168.0.28) # The IP addresses of the ELECT cluster nodes
+OSSServerNode="192.168.0.27" # The IP address of the OSS server node
 OSSServerPort=8000 # The port number of the OSS server node
-ClientNode="10.31.0.183" # The IP address of the client node (it can be the local node running the scripts)
-UserName="cc" # The user name of all the previous nodes
-sudoPasswd="" # The sudo password of all the previous nodes; we use this to install the required packages automatically; we assume all the nodes have the same user name. For the Chameleon cloud, please keep this as empty.
-PathToArtifact="/home/${UserName}/ELECT" # The path to the artifact folder; we assume all the nodes have the same path.
-PathToELECTExpDBBackup="/home/${UserName}/ELECTExpDBBackup" # The path to the backup folder for storing the loaded DB content; we assume all the nodes have the same path.
-PathToELECTLog="/home/${UserName}/ELECTLogs" # The path to the log folder for storing the experiment logs; we assume all the nodes have the same path.
-PathToELECTResultSummary="/home/${UserName}/ELECTResults" # The path to the result summary folder for storing the final experiment results; we assume all the nodes have the same path. 
+ClientNode="192.168.0.29" # The IP address of the client node (it can be the local node running the scripts)
+UserName="yjren" # The user name of all the previous nodes
+sudoPasswd="yjren" # The sudo password of all the previous nodes; we use this to install the required packages automatically; we assume all the nodes have the same user name. For the Chameleon cloud, please keep this as empty.
+PathToArtifact="/mnt/ssd/ELECT" # The path to the artifact folder; we assume all the nodes have the same path.
+PathToELECTExpDBBackup="/mnt/ssd/ELECTExpDBBackup" # The path to the backup folder for storing the loaded DB content; we assume all the nodes have the same path.
+PathToELECTLog="/mnt/ssd/ELECTLogs" # The path to the log folder for storing the experiment logs; we assume all the nodes have the same path.
+PathToELECTResultSummary="/mnt/ssd/ELECTResults" # The path to the result summary folder for storing the final experiment results; we assume all the nodes have the same path. 
 
 PathToELECTPrototype="${PathToArtifact}/src/elect"
 PathToYCSB="${PathToArtifact}/scripts/ycsb"
